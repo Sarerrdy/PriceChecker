35b61a59f98d613b82280ab57dd5292418cab351
Edmond Ina <sarerrdy4live@live.co.uk>
2023-08-07 06:27:09 +0100

    Add App Context

---
+++ PriceChecker/Models/Context/PriceCheckerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PriceChecker.Models.Context
+{
+    public class PriceCheckerContext : IdentityDbContext
+    {
+        public PriceCheckerContext(DbContextOptions<PriceCheckerContext> options) : base(options) { }
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Market> Markets { get; set; }
+        public DbSet<Seller> Sellers { get; set;}
+        public DbSet<History> Historys { get; set; }
+    }
+}
